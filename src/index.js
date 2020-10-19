46ef1fc551bd267364e230cfdb9e1b681bdd2b4b
Tessa Steffen <tessasofteng@gmail.com>
2020-10-19 15:54:27 -0500

    Login and registration both working. Registration logging correctly to the db.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
+import "./index.css";
+import {Plantd} from './Plantd';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Plantd />
+    </Router> 
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +18,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
