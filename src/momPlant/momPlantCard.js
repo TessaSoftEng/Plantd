4fca620241c7253e876cea0578b847cdbcef1cf2
Tessa Steffen <tessasofteng@gmail.com>
2020-10-21 14:19:24 -0500

    Started work on creation of mom and child plant cards

---
+++ src/momPlant/momPlantCard.js
@@ -0,0 +1,4 @@
+import React from "react"
+import "./momPlant.css"
+import { Card, CardBody, CardTitle, CardText, CardHeader, Container, Row, Col } from "reactstrap"
+import { Link } from "react-router-dom"
